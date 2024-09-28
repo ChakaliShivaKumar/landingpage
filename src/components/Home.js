aba6c735f10134a50a99edf3493c9c2ac83e6e5f
ChakaliShivaKumar <schakali@charlotte.edu>
2024-09-28 13:03:39 -0400

    commit1

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="container text-center main-home">
+        <div class="row">
+            <div class="col-lg-8">
+                <h1>LEZIT TRANSPORTS</h1>
+                <h5>a transportation hub….</h5>
+                <h5>One stop solution for all your transportation purposes</h5>
+            </div>
+            <div class="col-lg-4">
+                `<div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary" type="button">Book now</button>
+                    <button class="btn btn-primary" type="button">Vehicle rentals</button>
+                    <button class="btn btn-primary" type="button">Shuttle/commute transportation</button>
+                    <button class="btn btn-primary" type="button">Drivers</button>
+                    <button class="btn btn-primary" type="button">Logistics</button>
+                    <button class="btn btn-primary" type="button">Interstate/Intrastate transportation</button>
+                    <button class="btn btn-primary" type="button">Order delivery</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src={require('../images/TItle.png')} alt="" width="60" height="34"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Person (Passenger) transportation</a></li>
+            <li><a class="dropdown-item" href="#">Goods transportation</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Business</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Query</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Feedback</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
